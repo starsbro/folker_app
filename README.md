1ec7d2313f2b01c91a6e18d37295000316e94843
ensenginbaieer <lee.hsua@northeastern.edu>
2024-08-26 10:11:09 -0400

    SAM first commit

--- README.md
+++
@@ -1,6 +0,0 @@
-# folker_app
-first_app by flutter
-
-Main function:
-  1. Image loading and segmentation
-  2. add favorites
--- folker_app/android/app/src/main/kotlin/com/example/folker_app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.folker_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    
-}
